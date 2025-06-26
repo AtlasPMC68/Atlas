f0c7a6974e20ad69ccdfd3e3b9cbee2d662eddaf
AlexleQc <guea0902@usherbrooke.ca>
2025-06-25 21:06:04 -0400

    ATLAS-20: fix autoreload for frontend

--- Frontend-Atlas/vite.config.ts
+++ Frontend-Atlas/vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    host: true,         // 👈 autorise l'accès via IP externe (nécessaire dans Docker)
+    port: 5173,         // 👈 par défaut, mais explicite
+    watch: {
+      usePolling: true  // 👈 important pour que le bind mount détecte les changements !
+    }
+  }
 })
