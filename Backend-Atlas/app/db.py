46df0da2736db043d61684952ac9046cc7ba983d
AlexleQc <guea0902@usherbrooke.ca>
2025-06-25 22:32:17 -0400

    ATLAS-50: add type ignore

--- Backend-Atlas/app/db.py
+++ Backend-Atlas/app/db.py
@@ -1,6 +1,6 @@
 import os
-import psycopg2
-from dotenv import load_dotenv
+import psycopg2 # type: ignore
+from dotenv import load_dotenv # type: ignore
 
 # Charge les variables d'environnement depuis .env.dev
 load_dotenv(dotenv_path=".env.dev")
