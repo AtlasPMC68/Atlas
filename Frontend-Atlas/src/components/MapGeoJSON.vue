1a8a929598689a0f60c242908b6c599281c7b097
raphbouc <bour0703@usherbrooke.ca>
2025-07-02 13:00:53 -0400

    Atlas-60-feature-add-map-and-sample-geojson

---
+++ Frontend-Atlas/src/components/MapGeoJSON.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="map" style="height: 80vh; width: 100%;"></div>
+</template>
+
+<script setup>
+import { onMounted } from 'vue';
+import L from 'leaflet';
+
+onMounted(() => {
+  const map = L.map('map').setView([52.9399, -73.5491], 5);
+
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '© OpenStreetMap contributors'
+  }).addTo(map);
+
+  const geojson = {
+    type: "Feature",
+    geometry: {
+      type: "Polygon",
+      coordinates: [[
+        [-75, 50],
+        [-72, 50],
+        [-72, 52],
+        [-75, 52],
+        [-75, 50]
+      ]]
+    },
+    properties: {
+      name: "Zone Test Québec"
+    }
+  };
+
+  L.geoJSON(geojson).addTo(map);
+});
+</script>
---
+++ Frontend-Atlas/src/views/Map.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="min-h-screen w-full bg-white flex flex-col">
+    <h1 class="text-2xl font-bold text-center py-6">Carte démo</h1>
+    <MapGeoJSON />
+  </div>
+</template>
+
+<script setup>
+import MapGeoJSON from '../components/MapGeoJSON.vue';
+</script>
