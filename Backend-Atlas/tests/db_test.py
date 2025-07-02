b70960b7c6dbf29aca4770101ffaaaebc16ca366
raphbouc <bour0703@usherbrooke.ca>
2025-07-02 12:15:14 -0400

    remove comment

--- Backend-Atlas/tests/db_test.py
+++ Backend-Atlas/tests/db_test.py
@@ -3,21 +3,17 @@ import pytest
 from unittest.mock import patch, MagicMock
 from dotenv import load_dotenv
 
-from app.db import get_db_connection  # adapte selon ton arborescence
+from app.db import get_db_connection
 
 def test_get_db_connection_uses_env_variable():
     load_dotenv(dotenv_path=".env.dev")
 
     DATABASE_URL = os.getenv("DATABASE_URL")
-
-    # Patch psycopg2.connect pour ne pas réellement se connecter
     with patch("app.db.psycopg2.connect") as mock_connect:
         mock_conn = MagicMock()
         mock_connect.return_value = mock_conn
 
-        # Act
         conn = get_db_connection()
 
-        # Assert
         mock_connect.assert_called_once_with(DATABASE_URL)
         assert conn == mock_conn
