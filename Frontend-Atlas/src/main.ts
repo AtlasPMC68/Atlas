b33e49d0889d3c53bb51648b1e602461405c5eaa
raphbouc <bour0703@usherbrooke.ca>
2026-01-10 15:21:37 -0500

    AP-12-add-keycloak-auth

--- Frontend-Atlas/src/main.ts
+++ Frontend-Atlas/src/main.ts
@@ -3,12 +3,21 @@ import { createPinia } from "pinia";
 import App from "./App.vue";
 import { router } from "./router";
 import "./style.css";
+import keycloak from "./keycloak";
 
 const app = createApp(App);
 
-// Créer et configurer Pinia
 const pinia = createPinia();
 app.use(pinia);
-
 app.use(router);
+
+app.config.globalProperties.$keycloak = keycloak;
+
 app.mount("#app");
+
+keycloak
+  .init({ onLoad: "check-sso", pkceMethod: "S256" })
+  .then((authenticated) => {
+    console.log("Keycloak authenticated?", authenticated);
+  })
+  .catch((err) => console.error("Erreur Keycloak :", err));
