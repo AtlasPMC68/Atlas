eaecb7e00da33f2796f64047259d06149bd0a4ca
Gabriel Lamontagne <115102448+LamonGab@users.noreply.github.com>
2026-02-01 11:00:09 -0500

    Update main.py

--- Backend-Atlas/app/main.py
+++ Backend-Atlas/app/main.py
@@ -16,7 +16,7 @@ app.include_router(auth.router)
 app.include_router(user.router)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000", "http://localhost:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
