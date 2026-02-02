1f6e4bc341070dc33275c7fc4da80eb2aff97054
Gabriel L. <gabriel.labrecque@outlook.com>
2026-02-02 10:52:39 -0500

    Added benchmark logic

--- Backend-Atlas/tests/test_extraction_text.py
+++ Backend-Atlas/tests/test_extraction_text.py
@@ -45,6 +45,32 @@ def normalize_to_ascii_format(text: str) -> str:
     """Remove accents from words, then encore to ascii"""
     return unicodedata.normalize('NFKD', text).encode('ascii', 'ignore').decode('ascii')
 
+def check_for_perfect_match(actual: list[str], expected: list[str]):
+    # First try to match perfectly the words
+    for target in expected:
+        if target in actual:
+            expected.remove(target)
+            actual.remove(target)
+
+def check_for_partial_match(actual: list[str], expected: list[str]) -> float:
+    best_match_idx = -1
+    min_dist = float('inf')
+
+    for i, text in actual:
+
+        # Check if ASCII conversion matches
+        if text in expected:
+            min_dist = 0
+            best_match_idx = i
+            break
+
+
+        # Calculate weighted distance between words
+        current_dist = lev(found_ascii, target_ascii, **levenshtein_params)
+        if current_dist < min_dist:
+            min_dist = current_dist
+            best_match_idx = i
+
 # Load once in RAM during each test in THIS file. Simply adding
 # the file name as an argument to a function will inject the reference.
 @pytest.fixture(scope="module")
@@ -65,39 +91,35 @@ def levenshtein_params():
     ids=lambda val: val[0].name
 )
 def test_text_extraction(image_path, expected_text, levenshtein_params):
+
     assert image_path.exists()
     img = cv2.imread(str(image_path))
     found_texts, _ = extract_text(image=img, languages=['en', 'fr'], gpu_acc=False)
+
+    # Prepare text lists
     remaining_found = [res[1] for res in found_texts]
     remaining_expected = deepcopy(expected_text)
+    assert len(remaining_found) == len(remaining_expected)
 
-    # First try to match perfectly the words
-    for target in list(remaining_expected):
-        if target in remaining_found:
-            remaining_expected.remove(target)
-            remaining_found.remove(target)
+    # Check for perfect match. Pop values from arrays if found
+    check_for_perfect_match(actual=remaining_found, expected=remaining_expected)
+
+    # Convert remaining text to ASCII. Necessary to calculate the Levenshtein
+    # distance between the OCR text and the expected one
+    remaining_found_ascii = []
+    remaining_expected_ascii = []
+    for text in remaining_found:
+        remaining_found_ascii.append(normalize_to_ascii_format(text))
+    for text in remaining_expected:
+        remaining_expected_ascii.append(normalize_to_ascii_format(text))
+
+    # Check for partial ASCII match. Pop values from text with the closest distance.
+    distance, len = check_for_partial_match(actual=remaining_found_ascii, expected=remaining_expected_ascii)
+
 
     # Then pass again, and try to match as best as possible
     for target in list(remaining_expected):
         target_ascii = normalize_to_ascii_format(target)
-
-        best_match_idx = -1
-        min_dist = float('inf')
-
-        for i, found in enumerate(remaining_found):
-            found_ascii = normalize_to_ascii_format(found)
-
-            # Check if ASCII conversion matches
-            if target_ascii == found_ascii:
-                min_dist = 0
-                best_match_idx = i
-                break
-
-            # Calculate weighted distance between words
-            current_dist = lev(found_ascii, target_ascii, **levenshtein_params)
-            if current_dist < min_dist:
-                min_dist = current_dist
-                best_match_idx = i
 
         # Best match
         threshold = len(target_ascii) * 0.25
