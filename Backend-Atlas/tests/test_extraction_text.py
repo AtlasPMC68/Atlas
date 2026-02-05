9b3812b087b7559568b589ccc451640e704a8704
Gabriel L. <gabriel.labrecque@outlook.com>
2026-02-05 10:48:02 -0500

    v2 of the matching logic

--- Backend-Atlas/tests/test_extraction_text.py
+++ Backend-Atlas/tests/test_extraction_text.py
@@ -14,14 +14,11 @@ def get_image_paths():
     Locates the assets folder relative to this file and
     collects all images paths with supported extensions.
     """
+    extensions = ('.jpg', '.jpeg', '.png', '.bmp', '.tif', '.tiff', '.webp', '.ppm', '.pgm', '.pbm')
+
     # Path of the current test file
     current_dir = Path(__file__).parent
     assets_dir = current_dir / "assets"
-    print(assets_dir)
-    print("testing printing")
-
-    # Define supported extensions
-    extensions = ('.jpg', '.jpeg', '.png', '.bmp', '.tif', '.tiff', '.webp', '.ppm', '.pgm', '.pbm')
 
     images = []
     if assets_dir.exists():
@@ -37,39 +34,50 @@ def get_test_data():
     for img in images:
         expected = MAP_EXPECTED_TEXTS.get(img.stem)
         if expected:
-            # On crée un tuple (path, list) pour chaque test
+            # For each image, pull: (image path, word list)
             data.append((img, expected))
     return data
 
-def normalize_to_ascii_format(text: str) -> str:
-    """Remove accents from words, then encore to ascii"""
-    return unicodedata.normalize('NFKD', text).encode('ascii', 'ignore').decode('ascii')
+def normalize_array_to_ascii_format(text: list[str]) -> dict[str, str]:
+    """Replace all characters to ASCII format"""
 
-def check_for_perfect_match(actual: list[str], expected: list[str]):
-    # First try to match perfectly the words
-    for target in expected:
-        if target in actual:
-            expected.remove(target)
-            actual.remove(target)
+    res: dict[str, str] = {}
+    for word in text:
+        res[word] = (unicodedata
+                     .normalize('NFKD', word)
+                     .encode('ascii', 'ignore')
+                     .decode('ascii'))
+    return res
 
-def check_for_partial_match(actual: list[str], expected: list[str]) -> float:
-    best_match_idx = -1
-    min_dist = float('inf')
+def check_for_match(actual: list[str], expected: list[str]) -> dict[str, tuple[str, int]]:
 
-    for i, text in actual:
+    actual_ascii_dict = normalize_array_to_ascii_format(actual)
+    expected_ascii_dict = normalize_array_to_ascii_format(expected)
+    res: dict[str, tuple[str, float]] = {}
 
-        # Check if ASCII conversion matches
-        if text in expected:
-            min_dist = 0
-            best_match_idx = i
-            break
+    # Try to find the closest match for every read wor
+    for ocr_word, ocr_word_ascii in actual_ascii_dict.items():
 
+        min_dist: tuple[str, float] = ("a", 1000.0)
+        for expected_word, expected_word_ascii in expected_ascii_dict.items():
 
-        # Calculate weighted distance between words
-        current_dist = lev(found_ascii, target_ascii, **levenshtein_params)
-        if current_dist < min_dist:
-            min_dist = current_dist
-            best_match_idx = i
+            # Perfect match
+            if ocr_word == expected_word:
+                min_dist = (expected_word, 0.0)
+
+            # Pefect ASCII match
+            elif ocr_word_ascii == expected_word_ascii and min_dist[1] < 0.1:
+                min_dist = (expected_word, 0.1)
+
+            # Partial match
+            else:
+                tmp_dist = lev(ocr_word_ascii,expected_word_ascii,**levenshtein_params)
+                if tmp_dist < min_dist[1]:
+                    min_dist = (expected_word, tmp_dist)
+
+        # After checking all possible match, save the closest match
+        res[ocr_word] = min_dist
+
 
 # Load once in RAM during each test in THIS file. Simply adding
 # the file name as an argument to a function will inject the reference.
@@ -97,12 +105,15 @@ def test_text_extraction(image_path, expected_text, levenshtein_params):
     found_texts, _ = extract_text(image=img, languages=['en', 'fr'], gpu_acc=False)
 
     # Prepare text lists
-    remaining_found = [res[1] for res in found_texts]
-    remaining_expected = deepcopy(expected_text)
-    assert len(remaining_found) == len(remaining_expected)
+    remaining_ocr_words: list[str] = [res[1] for res in found_texts]
+    remaining_expected_words: list[str] = deepcopy(expected_text)
+    assert len(remaining_ocr_words) == len(remaining_expected_words)
 
     # Check for perfect match. Pop values from arrays if found
-    check_for_perfect_match(actual=remaining_found, expected=remaining_expected)
+    for i, read_word in enumerate(remaining_ocr_words):
+
+        res: bool = check_for_perfect_match(actual=read_word,, expected=remaining_expected)
+        if res == True:
 
     # Convert remaining text to ASCII. Necessary to calculate the Levenshtein
     # distance between the OCR text and the expected one
