d0616e498068e72203ef5d548657b8b2900595c2
z4cg <116285342+z4cg@users.noreply.github.com>
2026-04-24 16:32:33 -0400

    import duplicate resolve

--- Backend-Atlas/app/utils/color_in_legends_extraction.py
+++ Backend-Atlas/app/utils/color_in_legends_extraction.py
@@ -1,5 +1,5 @@
 import os
-from typing import Dict, List, Optional, Optional, Tuple
+from typing import Dict, List, Optional, Tuple
 import cv2
 import numpy as np
 from skimage.color import rgb2lab
