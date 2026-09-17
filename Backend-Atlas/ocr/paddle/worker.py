5ffc87378d256b622f5f12eef1d70a138905d877
RoxxCote <cotr3901@usherbrooke.ca>
2026-09-17 14:49:15 -0400

    t1

--- Backend-Atlas/ocr/paddle/worker.py
+++ Backend-Atlas/ocr/paddle/worker.py
@@ -23,7 +23,7 @@ _CACHED_OCR = None
 def get_ocr():
     global _CACHED_OCR
     if _CACHED_OCR is None:
-        _CACHED_OCR = PaddleOCR(use_angle_cls=True, lang="fr")
+        _CACHED_OCR = PaddleOCR(use_angle_cls=True, lang="fr", enable_mkldnn=False)
     return _CACHED_OCR
 
 
