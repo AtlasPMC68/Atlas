9ca8771b28fa0d4776381ac3f42cfe463e7fb49d
RoxxCote <cotr3901@usherbrooke.ca>
2026-09-17 13:30:13 -0400

    expected texte

--- Backend-Atlas/tests/utils/expected_text_results.py
+++ Backend-Atlas/tests/utils/expected_text_results.py
@@ -1,85 +1,14 @@
-
+# fmt: off
+# ruff: noqa
 MAP_EXPECTED_TEXTS = {
-    "1775_Quebec_NordUSA": [
-        "PROVINCE\nDE QUÉBEC", "Fleuve St-Laurent", "Québec", "Montgomery",
-        "Rivière Chaudière", "Arnold", "Montréal", "Rivière Richelieu",
-        "Saint-Jean", "Rivière Kennebec", "MAINE\n(MASS.)", "Fort Ticonderoga",
-        "NEW\nHAMPSHIRE", "Boston", "MASSACHUSETTS", "OCÉAN\nATLANTIQUE",
-        "L'invasion de la province de Québec en 1775"
-    ],
-    "Communautés_cries": [
-        "Communautés\ncries", "Whapmagoostui", "Chisasibi", "Wemindji",
-        "Eastmain", "Nemaska", "Waskaganish", "Mistissini",
-        "Oujé-Bougoumou", "Waswanipi"
-    ],
-    "genocide_Monde": [
-        "QUELQUES-UNS DES GÉNOCIDES ET CRIMES\nCONTRE L’HUMANITÉ DU 20E SIÈCLE",
-        "SEVERAL OF THE GENOCIDES AND CRIMES\nAGAINST HUMANITY OF THE 20TH CENTURY",
-        "Canada", "Holodomor", "Srebrenica", "Holocauste\nHolocaust",
-        "Arménie\nArmenia", "Rwanda", "Namibie\nNamibia", "Cambodge\nCambodia",
-        "Reconnu par le Canada / Recognized by Canada",
-        "Reconnu par l'ONU / Recognized by the United Nations",
-        "Génocide non reconnu par le Canada / Genocide not yet recognized by Canada",
-        "Génocide non reconnu par l'ONU / Genocide not yet recognized by United Nations"
-    ],
-    "Nouvelle-France1750": [
-        "La Nouvelle-France\nvers 1750", "Contrôle des territoires:", "France",
-        "Territoires cédés par la France à la Grande-Bretagne par le traité d'Utrecht de 1713",
-        "Grande-Bretagne", "Espagne", "Baie d'Hudson", "Fort Bourbon", "Fort Dauphin",
-        "Fort La Reine", "Pays d'en Hauts", "Fort St-Pierre", "Fort Beauharnois",
-        "Fort Michilimakinac", "Fort Détroit", "Fort Frontenac", "CANADA",
-        "Tadoussac (1600)", "Québec (1608)", "Trois-Rivières (1634)", "Montréal (1642)",
-        "Fort Richelieu", "Fort Chambly", "ACADIE", "Île St-Jean", "Isle Royale",
-        "Louisbourg (1719)", "Port-Royal (1605)", "Terre-Neuve", "Plaisance (1662)",
-        "Boston (1630)", "New York (1626)", "Philadelphie (1681)", "Baltimore (1729)",
-        "Haute-Louisiane\n(Pays des Illinois)", "Fort Duquesne", "Fort Orléans",
-        "Fort de Chartres", "Fort St-Louis", "LOUISIANE", "Basse-\nLouisiane",
-        "Charleston (1680)", "Savannah (1733)", "Fort Rosalie", "Fort Toulouse",
-        "Baton Rouge (1720)", "Mobile (1702)", "Biloxi (1699)", "La Nouvelle-Orléans (1718)",
-        "Océan\nAtlantique", "Océan\nPacifique", "Golfe du Mexique"
-    ],
-    "Progress_wehrmacht_lux_May_1940": [
-        "Progress of the Wehrmacht\nduring 10th May 1940", "Belgium", "Germany",
-        "2. PzD", "1. PzD", "10. PzD", "Vianden", "Ettelbrück", "Larochette",
-        "Echternach", "Mersch", "Luxemburg", "France"
-    ],
-    "Quebec_1791": [
-        "Carte de l'Acte constitutionnel de 1791", "Baie d'Hudson", "Mer du Labrador",
-        "TERRE DE RUPERT", "HAUT-CANADA", "BAS-CANADA", "Québec", "Fleuve Saint-Laurent",
-        "Montréal", "ÎLE\nSAINT-JEAN", "ÎLE\nDU\nCAP-BRETON", "TERRE-NEUVE",
-        "NOUVEAU-\nBRUNSWICK", "NOUVELLE-\nÉCOSSE", "OCÉAN\nATLANTIQUE", "Lac Supérieur",
-        "Lac Michigan", "Lac Huron", "Kingston", "Lac Ontario", "Toronto", "Lac Érié",
-        "Boston", "New York", "ÉTATS-UNIS", "LOUISIANE", "Fleuve Mississippi",
-        "Rivière Missouri", "Territoire contesté", "Frontière définie", "Frontière approximative"
-    ],
-    "Quebec_1800": [
-        "Les principales routes du commerce des fourrures vers 1800",
-        "Compagnie de la baie d'Hudson", "Compagnie du Nord-Ouest", "Frontière définie",
-        "Frontière approximative", "Baie d'Hudson", "Fort Churchill", "Fort York",
-        "Fort Severn", "Fort Albany", "TERRE DE RUPERT", "Fort Eastmain", "Fort Rupert",
-        "Fort Moose", "Fort Cumberland", "Lac Winnipeg", "Lac Supérieur",
-        "PROVINCE\nDE QUÉBEC", "Québec", "Montréal", "Fleuve Saint-Laurent",
-        "ÎLE\nSAINT-JEAN", "ÎLE\nDU\nCAP-BRETON", "TERRE-NEUVE", "NOUVEAU-\nBRUNSWICK",
-        "NOUVELLE-\nÉCOSSE", "OCÉAN\nATLANTIQUE", "Lac Michigan", "Lac Huron",
-        "Lac Érié", "Lac Ontario", "Boston", "New York", "LOUISIANE"
-    ],
-    "Quebec_Traite1783": [
-        "Traité de Paris de 1783", "Baie\nd'Hudson", "Mer du Labrador",
-        "TERRE DE RUPERT", "PROVINCE\nDE QUÉBEC", "TERRE-\nNEUVE",
-        "ÎLE\nSAINT-JEAN", "NOUVELLE-\nÉCOSSE", "Québec", "Montréal",
-        "Fleuve Saint-Laurent", "Boston", "New York", "ÉTATS-UNIS",
-        "LOUISIANE", "FLORIDE", "La Nouvelle-\nOrléans", "Golfe\ndu Mexique",
-        "OCÉAN\nATLANTIQUE", "Lac Supérieur", "Lac Michigan", "Lac Huron",
-        "Lac Érié", "Lac Ontario", "Rivière Missouri", "Rivière\nMississippi",
-        "Rivière Ohio", "Madeleine", "Saint-Pierre",
-        "Possessions anglaises", "Province de Québec", "Autres territoires",
-        "Possessions espagnoles", "Louisiane", "Floride",
-        "Possession américaine", "États-Unis",
-        "Possession française", "Saint-Pierre et Miquelon",
-        "Territoire contesté", "Frontière définie", "Frontière approximative"
-    ],
-    "Sahel_Afrique": [
-        "Mauritanie", "Sénégal", "Mali", "Burkina\nFaso", "Niger",
-        "Tchad", "Soudan", "Érythrée", "Algérie", "Nigeria", "Éthiopie"
-    ],
+    "1775_Quebec_NordUSA": ["N", "O", "E", "PROVINCE\nDE QUÉBEC", "Fleuve St-Laurent", "Québec", "Montgomery", "Rivière Chaudière", "Arnold", "Montréal", "Rivière Richelieu", "Saint-Jean", "Montgomery", "Rivière Kennebec", "MAINE\n(MASS.)", "Fort Ticonderoga", "NEW\nHAMPSHIRE", "OCÉAN\nATLANTIQUE", "0", "50", "100", "Boston", "kilomètres", "MASSACHUSETTS", "L'invasion de la province de Québec en 1775", "3"],
+    "Communautés_cries": ["Communautés\ncries", "Whapmagoostui", "Chisasibi", "Wemindji", "Eastmain", "Nemaska", "Waskaganish", "Mistissini", "Oujé-Bougoumou", "Waswanipi"],
+    "genocide_Monde": ["QUELQUES-UNS DES GÉNOCIDES ET CRIMES\nCONTRE L’HUMANITÉ DU 20E SIÈCLE", "SEVERAL OF THE GENOCIDES AND CRIMES\nAGAINST HUMANITY OF THE 20TH CENTURY", "Canada", "Holodomor", "Holocauste\nHolocaust", "Srebrenica", "Arménie\nArmenia", "Rwanda", "Cambodge\nCambodia", "Namibie\nNamibia", "Reconnu par le Canada / Recognized by Canada", "Reconnu par l'ONU / Recognized by the United Nations", "Génocide non reconnu par le Canada / Genocide not yet recognized by Canada", "Génocide non reconnu par l'ONU / Genocide not yet recognized by United Nations"],
+    "Nouvelle-France1750": ["La Nouvelle-France\nvers 1750", "Baie\nd'Hudson", "Contrôle des territoires:", "France", "Territoires cédés par la France\nà la Grande-Bretagne par le\ntraité d'Utrecht de 1713", "Grande-Bretagne", "Espagne", "Fort Bourbon", "Fort Dauphin", "Fort La Reine", "Pays d'en Hauts", "CANADA", "Terre-\nNeuve", "Plaisance (1662)", "Tadoussac (1600)", "Isle St-Jean", "Isle Royale", "Louisbourg (1719)", "Fort St-Pierre", "Québec (1608)", "Trois-Rivières (1634)", "Montréal (1642)", "ACADIE", "Fort Richelieu", "Port-Royal (1605)", "Fort Chambly", "Fort Beauharnois", "Fort Michilimakinac", "Fort Frontenac", "Fort Détroit", "Boston (1630)", "New York (1626)", "Fort Duquesne", "Haute-Louisiane\n(Pays des Illinois)", "Philadelphie (1681)", "Baltimore (1729)", "Fort Orléans", "Fort de Chartres", "Fort St-Louis", "Océan\nAtlantique", "LOUISIANE", "Basse-\nLouisiane", "Charleston (1680)", "Savannah (1733)", "Fort Rosalie", "Fort Toulouse", "Baton Rouge (1720)", "Mobile (1702)", "Biloxi (1699)", "La Nouvelle-Orléans (1718)", "Golfe du Mexique", "Océan\nPacifique"],
+    "Progress_wehrmacht_lux_May_1940": ["Progress of the Wehrmacht\nduring 10th May 1940", "Belgium", "Germany", "2. PzD", "Vianden", "1. PzD", "Ettelbrück", "Larochette", "10. PzD", "Echternach", "Mersch", "Luxemburg", "France"],
+    "Quebec_1791": ["Mer du Labrador", "Baie\nd'Hudson", "TERRE DE RUPERT", "TERRE-\nNEUVE", "BAS-CANADA", "HAUT-CANADA", "Lac Supérieur", "Québec", "Fleuve Saint-Laurent", "ÎLE\nSAINT-\nJEAN", "ÎLE DU\nCAP-\nBRETON", "NOUVEAU-\nBRUNSWICK", "Montréal", "NOUVELLE-\nÉCOSSE", "OCÉAN\nATLANTIQUE", "Lac Michigan", "Lac Huron", "Kingston", "Lac Ontario", "Toronto", "Lac Érié", "Boston", "New York", "ÉTATS-UNIS", "Rivière Missouri", "LOUISIANE", "Fleuve Mississippi", "Territoire contesté", "Frontière définie", "Frontière approximative", "Carte de l'Acte constitutionnel de 1791", "3"],
+    "Quebec_1800": ["Compagnie de la baie d'Hudson", "Compagnie du Nord-Ouest", "Frontière définie", "Frontière approximative", "Baie\nd'Hudson", "Fort\nChurchill", "Fort York", "Fort Severn", "TERRE DE RUPERT", "Fort Albany", "Fort Eastmain", "Fort Rupert", "Fort Moose", "Fort\nCumberland", "Lac\nWinnipeg", "Lac Supérieur", "PROVINCE\nDE QUÉBEC", "Québec", "Fleuve Saint-Laurent", "ÎLE\nSAINT-\nJEAN", "TERRE-\nNEUVE", "ÎLE DU\nCAP-\nBRETON", "NOUVEAU-\nBRUNSWICK", "Montréal", "NOUVELLE-\nÉCOSSE", "OCÉAN\nATLANTIQUE", "Lac Michigan", "Lac Huron", "Lac\nOntario", "Lac Érié", "Boston", "New York", "Rivière\nMissouri", "Fleuve\nMississippi", "LOUISIANE", "Les principales routes du commerce des fourrures vers 1800", "6"],
+    "Quebec_Traite1783": ["Mer du Labrador", "Baie\nd'Hudson", "TERRE DE RUPERT", "TERRE-\nNEUVE", "PROVINCE\nDE QUÉBEC", "Miquelon", "Saint-Pierre", "Fleuve Saint-Laurent", "ÎLE\nSAINT-\nJEAN", "Québec", "NOUVELLE-\nÉCOSSE", "Montréal", "Lac Supérieur", "Lac Michigan", "Lac Huron", "Lac\nOntario", "Lac Érié", "Boston", "New York", "Possessions anglaises", "Province de Québec", "Autres territoires", "Possessions espagnoles", "Louisiane", "Floride", "Possession américaine", "États-Unis", "Possession française", "Saint-Pierre et Miquelon", "OCÉAN\nATLANTIQUE", "Rivière Ohio", "Rivière\nMissouri", "Fleuve\nMississippi", "ÉTATS-UNIS", "LOUISIANE", "La Nouvelle-\nOrléans", "Golfe\ndu Mexique", "FLORIDE", "Territoire contesté", "Frontière définie", "Frontière approximative", "Traité de Paris de 1783", "5"],
+    "Sahel_Afrique": ["Algérie", "Mauritanie", "Sénégal", "Mali", "Burkina\nFaso", "Niger", "Nigeria", "Tchad", "Soudan", "Érythrée", "Éthiopie"],
 }
+# fmt: on
