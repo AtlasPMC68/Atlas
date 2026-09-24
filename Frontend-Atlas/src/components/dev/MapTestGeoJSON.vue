b192faeff381c74a86ec6f7cff7fa63a5924910b
z4cg <116285342+z4cg@users.noreply.github.com>
2026-09-24 11:16:51 -0400

    merge gab

--- Frontend-Atlas/src/components/dev/MapTestGeoJSON.vue
+++ Frontend-Atlas/src/components/dev/MapTestGeoJSON.vue
@@ -469,7 +469,10 @@ function handleMapClick(e) {
 }
 async function loadGeoBorders() {
   if (!map) return;
-  const filename = "/geojson/geoBoundaries-ITA-ADM0_simplified.geojson";
+  const filename = [
+    "/geojson/geoBoundaries-ITA-ADM0_simplified.geojson",
+    "/geojson/geoBoundaries-CAN-ADM1_simplified.geojson",
+  ];
 
   try {
     const res = await fetch(filename);
